07ca96deb047819f67e2a527bf03078b36946f6f
donaldwongsuisang <donaldwongsuisang@gmail.com>
2024-04-25 19:08:31 +0800

    apply mySQL database

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
-const restaurants = require('./public/jsons/restaurant.json').results
+//const restaurants = require('./public/jsons/restaurant.json').results
 
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public'))
+
+const db = require('./models')
+const Restaurant = db.Restaurant
+
 
 app.get('/', (req, res) => {
   res.redirect('/restaurants')
@@ -15,15 +19,23 @@ app.get('/', (req, res) => {
 
 app.get('/restaurants', (req, res) => {
 const keyword = req.query.search?.trim()
-const matchedRestaurants = keyword ? restaurants.filter((rest) => 
-Object.values(rest).some((property) => {
-  if (typeof property === 'string'){
-    return property.toLowerCase().includes(keyword.toLowerCase())
-  }
-  return false
-})
-):restaurants
-  res.render('index', {restaurants: matchedRestaurants, keyword})
+
+return Restaurant.findAll({
+		attributes: ['id', 'name','name_en', 'category','image','location', 'phone','google_map','rating','description'],
+		raw: true
+	})
+  .then((restaurants) => {
+    const matchedRestaurants = keyword ? restaurants.filter((rest) => 
+    Object.values(rest).some((property) => {
+      if (typeof property === 'string')
+      {
+        return property.toLowerCase().includes(keyword.toLowerCase())
+      }
+      return false
+    })
+    ):restaurants
+    res.render('index', {restaurants: matchedRestaurants, keyword})
+  })
 })
 
 app.listen(port, () => {
@@ -32,6 +44,11 @@ app.listen(port, () => {
 
 app.get('/restaurant/:id', (req, res) => {
   const id = req.params.id
-  const restaurant = restaurants.find((rest) => rest.id.toString() === id)
-  res.render('detail', {restaurant})
+  return Restaurant.findByPk(id, {
+		attributes: ['id', 'name','name_en', 'category','image','location', 'phone','google_map','rating','description'],
+		raw: true
+	})
+  .then((restaurantid) => res.render('detail',{restaurantid}))
+  // const restaurant = restaurants.find((rest) => rest.id.toString() === id)
+  // res.render('detail', {restaurant})
 })
