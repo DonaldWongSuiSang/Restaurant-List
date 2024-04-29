5a8e0b8aa58e716b4bce703b7558f24f4d41b628
donaldwongsuisang <donaldwongsuisang@gmail.com>
2024-04-29 12:53:48 +0800

    build: edit & delete function

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public'))
+const methodOverride = require('method-override')
 
 const db = require('./models')
 const Restaurant = db.Restaurant
 
 app.use(express.urlencoded({ extended: true }))
-// app.use(methodOverride('_method'))
+app.use(methodOverride('_method'))
 
 
 app.get('/', (req, res) => {
@@ -46,7 +47,6 @@ app.get('/restaurant/:id', (req, res) => {
 		raw: true
 	})
   .then((restaurantid) => res.render('detail',{restaurantid}))
-
 })
 
 app.get('/restaurants/new', (req, res) => {
@@ -55,7 +55,6 @@ app.get('/restaurants/new', (req, res) => {
 
 app.post('/restaurants', (req, res) => {
 	const newRestaurant = req.body
-
   return Restaurant.create({    
       name: newRestaurant.name,
       name_en: newRestaurant.name_en,
@@ -71,8 +70,40 @@ app.post('/restaurants', (req, res) => {
         .catch((err) => console.log(err))
 })
 
+app.get('/restaurant/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findByPk(id, {
+		attributes: ['id', 'name','name_en', 'category','image','location', 'phone','google_map','rating','description'],
+		raw: true
+	})
+  .then((restaurantid) => res.render('edit',{restaurantid}))
+})
+
+app.put('/restaurant/:id', (req, res) => {
+  const id = req.params.id
+  const body = req.body
+  console.log(body)
+  return Restaurant.update({
+      name: body.name,
+      name_en: body.name_en,
+      category: body.category,
+      image: body.image,
+      location: body.location,
+      phone: body.phone,
+      google_map: body.google_map,
+      rating: body.rating,
+      description: body.description
+  }, {where: { id }})
+          .then(() => res.redirect(`/restaurant/${id}`))
+	})
+
+
+app.delete('/restaurant/:id', (req, res) => {
+  const id = req.params.id
+    return Restaurant.destroy({where: {id}})
+            .then(() => res.redirect('/restaurants'))
+})
 
 app.listen(port, () => {
   console.log(`express server is running on http://localhost:${port}`)
 })
-
